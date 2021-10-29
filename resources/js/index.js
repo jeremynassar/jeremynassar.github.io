3ffae99e326b74871041fcf6dc9cb2b16431f07e
Gunduzhazal <87630881+Gunduzhazal@users.noreply.github.com>
2021-10-29 19:08:23 +0300

    function

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,11 +1,18 @@
-const task= new TaskManager();
+const newTaskForm = document.querySelector('#newTaskForm');
+newTaskForm.addEventListener('submit', (event) => {
 
-let addButton = document.getElementById('addbtn');
-let deleteButton = document.getElementById('deletebtn');
+            event.preventDefault();
 
-function showTask (){
-    
-}
+            const newTaskNameInput = document.querySelector('#newTaskNameInput');
 
 
 
+
+            const task = new TaskManager();
+
+            let addButton = document.getElementById('addbtn');
+            let deleteButton = document.getElementById('deletebtn');
+
+            function showTask() {
+
+            }
